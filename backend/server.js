5c977bf6e49df327302b235ef6da989d60d3a419
MohamedBenArbi <mohamedbenarbi01@gmail.com>
2023-02-12 17:26:37 +0100

    DB config created config directory added DB config file

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,13 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const dotenv = require('dotenv').config()
 const userRoutes = require('./routes/userRoutes')
 const taskRoutes = require('./routes/taskRoutes')
+const { connectDB } = require('./config/db')
 
 const port = process.env.PORT || 5000 
 
-
+connectDB()
 const app = express()
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
