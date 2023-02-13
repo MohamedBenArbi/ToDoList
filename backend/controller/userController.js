1046bc3cd8d496d4c602b07a9bf0e822a1910806
MohamedBenArbi <mohamedbenarbi01@gmail.com>
2023-02-13 21:42:50 +0100

    login controller added + hashed pass compare

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken')
 
 
 const register = asyncHandler(async (req,res)=>{
-    
+    let token 
     const {name,email,password} = req.body
 
     if(!name || !email || !password){
@@ -15,31 +15,68 @@ const register = asyncHandler(async (req,res)=>{
         throw new Error("Please enter all infos")
     }
 
-    const user = await userModel.findOne({email})
+    const userExist = await userModel.findOne({email})
 
-    if(user){
+    if(userExist){
         res.status(400)
         throw new Error("email already used")
     }
-        
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newUser = await userModel.create({name,email,password:hashedPassword})
+        const user = await userModel.create({name,email,password:hashedPassword})
+
+        token = jwt.sign({id},process.env.JWT_SECRET)
          
-        if(newUser){
-            res.status(201).json(newUser)
+        if(user){
+            res.status(201).json({
+                _id : user.id , 
+                name : user.name , 
+                email : user.email,
+                token:generateToken(user._id)
+            })
+        }else{
+            res.status(400)
+            throw new Error('User wasnt created ')
         }
 
 })
 
 
 const login = asyncHandler (async (req,res)=>{
-    res.json({"Message" : "login"})
+    const {email,password} = req.body
+
+    if(!email || !password){
+        res.status(400)
+        throw new Error('Please fill all fileds')
+    }
+
+    const user = await userModel.findOne({email}) 
+
+    if(user && await bcrypt.compare(password,user.password)){
+     
+        res.status(200).json({
+            _id : user.id , 
+            name : user.name , 
+            email : user.email,
+            token:generateToken(user._id)
+        })
+
+    }else{
+        throw new Error('Invalid email or wrong password')
+    }
+
+
+
 })
 
 const getme = asyncHandler (async (req,res)=>{
     res.json({"Message" : "login"})
 })
+
+const generateToken = (id) =>{
+    return jwt.sign({id},process.env.JWT_SECRET)
+}
 
 
 module.exports = {
